26ca8b9e8aed5c92a042ded8c81d6c75dabeb1a3
doktoric <doktoricsrichard@gmail.com>
2014-07-11 12:52:31 +0200

    added deploy sh file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
-FROM       ubuntu:14.04
+FROM tcnksm/rbenv
 MAINTAINER SequenceIQ
+
+RUN locale-gen en_US.UTF-8
+
+ENV LANG en_US.UTF-8
+ENV LC_ALL en_US.UTF-8
 
 RUN \
   apt-get update && \
@@ -9,3 +14,8 @@ RUN \
 RUN pip install mkdocs
 
 COPY bootstrap /usr/local/lib/python2.7/dist-packages/mkdocs/themes/bootstrap
+
+ADD deploy-doc.sh /etc/deploy-doc.sh
+RUN chmod +x /etc/deploy-doc.sh
+
+CMD "/etc/deploy-doc.sh"
---
+++ deploy-doc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+: ${KEY?'missing github private key do deploy docker run -e KEY=XXXX'}
+
+[ -n "$DEBUG" ] && echo debug on ... && set -x
+
+: ${COMMIT_NAME:=jenkins}
+: ${COMMIT_EMAIL:=jenkins@sequenceiq.com}
+: ${PROJECT:=jenkins@sequenceiq.com}
+
+# private github key comes from env variable KEY
+# docker run -e KEY=XXXX
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+
+# switch off debug to hide private key
+set +x
+echo $KEY|base64 -d> /root/.ssh/id_rsa
+[ -n "$DEBUG" ] && echo debug on ... && set -x
+
+chmod 600 /root/.ssh/id_rsa
+
+# saves githubs host to known_hosts
+ssh -T -o StrictHostKeyChecking=no  git@github.com
+
+git config --global user.name "$COMMIT_NAME"
+git config --global user.email "$COMMIT_EMAIL"
+
+git clone git@github.com:sequenceiq/$PROJECT.git /tmp/$PROJECT
+mkdir /tmp/$PROJECT-temp
+cd /tmp/$PROJECT
+mkdocs build
+cp -R /tmp/cloudbreak/site /tmp/$PROJECT-temp
+git checkout gh-pages
+cp -R /tmp/$PROJECT-temp/site/ .
+git add -A
+git commit -m "updated documentation"
+git push origin gh-pages
